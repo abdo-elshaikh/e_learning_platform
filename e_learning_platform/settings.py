fea6f5b244b0e0bd76f07adf130105d26173929f
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-12-25 00:35:34 +0200

    Update static file paths in settings and adjust Vercel configuration

--- e_learning_platform/settings.py
+++ e_learning_platform/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # static files
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # media files
