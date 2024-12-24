fd8478c88c4d89b9195db140315b1ca2140297ae
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-12-25 00:56:08 +0200

    Revert BASE_DIR assignment to use os.path.dirname for compatibility

--- e_learning_platform/settings.py
+++ e_learning_platform/settings.py
@@ -3,8 +3,8 @@ from django.contrib.messages import constants as message_constants
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
